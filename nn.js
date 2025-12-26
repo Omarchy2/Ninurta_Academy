f625699e868c60d950edf9e1d38f48ee491c54a6
Omarchy2 <otgamerx26@gmail.com>
2025-12-26 18:49:23 +0300

    first

---
+++ nn.js
@@ -0,0 +1,89 @@
+/*---toggle icon navbar---*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+/*scroll section active link*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href* = ' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /*---sticky navbar---*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*---############---*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+
+
+
+ScrollReveal({
+    /*reset: true,*/
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .potfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['English & Programming Courses', 'Scholorships', 'Academic Research', 'Legal & Literary Translation', 'Web Development'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name")
+const email = document.getElementById("email")
+const phone = document.getElementById("phone")
+const subject = document.getElementById("subject")
+const mess = document.getElementById("message")
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "ninurtaacademy@gmail.com",
+        Password: "06AFBB09A24E50B9731B21DB4B8D62017972",
+        To: 'ninurtaacademy@gmail.com',
+        From: "ninurtaacademy@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then(
+        message => alert(message)
+    );
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail()
+});
